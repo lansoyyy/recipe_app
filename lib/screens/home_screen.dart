2f28bba38b3d1055d71082f04c49a7c048ea5bf4
Lans12345 <olanalans12345@gmail.com>
2024-07-20 07:13:18 +0800

    added home ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_app/screens/login_screen.dart';
+import 'package:recipe_app/screens/tabs/home_tab.dart';
+import 'package:recipe_app/utils/colors.dart';
+import 'package:recipe_app/widgets/logout_widget.dart';
+import 'package:recipe_app/widgets/text_widget.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const HomeTab(),
+    const Text('My Recipe',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    const Text('Favorites',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    const Text('Profile',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  List labels = ['Home', 'My Recipes', 'Favorites', 'Profile'];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primary,
+        title: TextWidget(
+          text: labels[_selectedIndex],
+          fontSize: 18,
+          color: Colors.white,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              logout(context, const LoginScreen());
+            },
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'My Recipe',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: primary,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
---
+++ lib/screens/tabs/home_tab.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:recipe_app/utils/colors.dart';
+import 'package:recipe_app/widgets/text_widget.dart';
+
+class HomeTab extends StatefulWidget {
+  const HomeTab({super.key});
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  final searchController = TextEditingController();
+  String nameSearched = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 0.5,
+                  ),
+                  borderRadius: BorderRadius.circular(100)),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: TextFormField(
+                  style: const TextStyle(
+                      color: Colors.black, fontFamily: 'Regular', fontSize: 14),
+                  onChanged: (value) {
+                    setState(() {
+                      nameSearched = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      hintText: 'Search Recipe',
+                      hintStyle: TextStyle(fontFamily: 'Regular', fontSize: 18),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      )),
+                  controller: searchController,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 175,
+                    child: Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 125,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(
+                                10,
+                              ),
+                              topRight: Radius.circular(
+                                10,
+                              ),
+                            ),
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, right: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  TextWidget(
+                                    text: 'Samosa',
+                                    fontSize: 18,
+                                    fontFamily: 'Bold',
+                                    color: primary,
+                                  ),
+                                  TextWidget(
+                                    text: 'Description',
+                                    fontSize: 12,
+                                    fontFamily: 'Regular',
+                                  ),
+                                ],
+                              ),
+                              TextWidget(
+                                text: '15 mins',
+                                fontSize: 28,
+                                fontFamily: 'Bold',
+                                color: primary,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
